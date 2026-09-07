e4d35256eb8f4715b40055897aa3643006663291
Ali Albakri <m.vy@hotmail.com>
2026-09-07 21:06:23 +0100

    Add staging network module

---
+++ infra/modules/network/variables.tf
@@ -0,0 +1,10 @@
+variable "name" {
+  description = "Name prefix for network resources."
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC."
+  type        = string
+  default     = "10.20.0.0/16"
+}
